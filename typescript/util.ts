920b36d5975af14f84c3037a20a807562e8ea45d
Josh Karpel <josh.karpel@gmail.com>
2020-11-27 12:40:48 -0500

    types!

--- typescript/util.ts
+++ typescript/util.ts
@@ -1,9 +1,9 @@
 import fs = require('fs');
 
-export function printSolution (day: number, part: number, solution: number) {
+export function printSolution (day: number, part: number, solution: number): void {
   console.log(`Day ${day}, Part ${part} => ${solution}`)
 }
 
-export function readFileLines (path: string): string[] {
+export function readFileLines (path: string): ReadonlyArray<string> {
   return fs.readFileSync(path, 'utf8').trimEnd().split('\n')
 }
