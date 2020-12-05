d82922fe71a55a3f47141ec9a00d9b51457b5e67
JoshKarpel <josh.karpel@gmail.com>
2020-12-05 09:13:05 -0600

    pre-commit

--- watch.js
+++ watch.js
@@ -5,6 +5,6 @@ const concurrently = require('concurrently')
 concurrently(
   [
     { command: 'tsc -w --preserveWatchOutput', prefixColor: 'blue', name: 'tsc' },
-    { command: `nodemon -L dist/day_${process.argv[2]}.js`, prefixColor: 'magenta', name: 'advent' }
-  ]
+    { command: `nodemon -L dist/day_${process.argv[2]}.js`, prefixColor: 'magenta', name: 'advent' },
+  ],
 )
