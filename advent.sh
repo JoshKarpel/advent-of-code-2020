2da016cde2b2474885d534bc3a4f53b8e992d952
JoshKarpel <josh.karpel@gmail.com>
2020-12-13 13:32:04 -0600

    advent.sh can run all days

--- advent.sh
+++ advent.sh
@@ -3,4 +3,12 @@
 set -e
 
 yarn run tsc
-node "dist/day_${1}.js"
+
+if [[ "${1}" == "all" ]]; then
+  for script in dist/day_*.js; do
+    node "${script}"
+    echo
+  done
+else
+  node "dist/day_${1}.js"
+fi
