7ba06b258c5a0220e2ef1ec471757d508d7aadf5
Josh Karpel <josh.karpel@gmail.com>
2020-12-22 11:55:14 -0500

    Solve Day 19 (#5)
    * refactor day 19 part 1
    
    * solve day 19 part 2
    
    * refactor day 19

--- typescript/day_19.ts
+++ typescript/day_19.ts
@@ -1,17 +1,72 @@
 import { printSolution, readFile } from './util'
 
-function part1 (rules: Map<string, RegExp>, messages: Array<string>): number {
-  const ruleZero = rules.get('0')
-  if (ruleZero === undefined) {
-    throw new Error('no rule 0!')
-  }
+type RawRule = Array<string>
+type RawRules = Map<string, RawRule>;
+
+function solve (
+  rules: RawRules,
+  messages: Array<string>,
+  regExpConstructor: (rules: RawRules, rule: Array<string>) => string,
+): number {
+  const ruleMap = new Map(
+    Array.from(rules.entries())
+      .map(([n, rule]) => [n, regExpConstructor(rules, rule)]),
+  )
+
+  const ruleZero = getRuleZero(ruleMap)
 
   return messages.filter(message => message.match(ruleZero)).length
 }
 
-const [rawRules, rawMessages] = readFile('data/day_19.txt').split('\n\n')
-const rules: Map<string, Array<string>> = new Map(
-  rawRules
+function constructRegExpSource (rules: RawRules, rule: RawRule): string {
+  const parts = [...rule]
+  while (parts.some(c => rules.has(c))) {
+    for (const [idx, part] of parts.entries()) {
+      if (rules.has(part)) {
+        parts.splice(idx, 1, '(', ...rules.get(part) || part, ')')
+        break
+      }
+    }
+  }
+  return parts.join('')
+}
+
+function constructRegExpSource2 (rules: RawRules, rule: RawRule): string {
+  const parts = [...rule]
+  while (parts.some(c => rules.has(c))) {
+    for (const [idx, part] of parts.entries()) {
+      if (rules.has(part)) {
+        if (part === '8') {
+          parts.splice(idx, 1, '(', ...rules.get(part) || part, ')+')
+        } else if (part === '11') {
+          const [a, b] = rules.get(part) || ''
+          const possibilities = Array.from(Array(7).keys())
+            .slice(1)
+            .map(n => Array(n).fill(a).concat(Array(n).fill(b)))
+            .flatMap(p => ['|', '(', ...p, ')'])
+            .slice(1)
+          parts.splice(idx, 1, '(', ...possibilities, ')')
+        } else {
+          parts.splice(idx, 1, '(', ...rules.get(part) || part, ')')
+        }
+        break
+      }
+    }
+  }
+  return parts.join('')
+}
+
+function getRuleZero (ruleMap: Map<string, string>): RegExp {
+  const ruleZeroSource = ruleMap.get('0')
+  if (ruleZeroSource === undefined) {
+    throw new Error('no rule 0!')
+  }
+  return RegExp(`^${ruleZeroSource}$`)
+}
+
+const [rawestRules, rawMessages] = readFile('data/day_19.txt').split('\n\n')
+const rawRules: RawRules = new Map(
+  rawestRules
     .split('\n')
     .map(line => {
       const [n, rawRule] = line.split(': ')
@@ -26,21 +81,7 @@ const rules: Map<string, Array<string>> = new Map(
     },
     ),
 )
-const rules2: Array<[string, RegExp]> = Array.from(rules.entries())
-  .map(([n, rule]) => {
-    const parts = [...rule]
-    while (parts.some(c => rules.has(c))) {
-      for (const [idx, part] of parts.entries()) {
-        if (rules.has(part)) {
-          parts.splice(idx, 1, '(', ...rules.get(part) || part, ')')
-          break
-        }
-      }
-    }
-    return [n, RegExp(`^${parts.join('')}$`)]
-  })
-const ruleMap = new Map(rules2)
-
 const messages = rawMessages.split('\n')
 
-printSolution(19, 1, part1(ruleMap, messages))
+printSolution(19, 1, solve(rawRules, messages, constructRegExpSource))
+printSolution(19, 2, solve(rawRules, messages, constructRegExpSource2))
