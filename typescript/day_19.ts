538af90515fc37a5fea6c2b591775f880851cb0b
Josh Karpel <josh.karpel@gmail.com>
2020-12-22 11:14:41 -0500

    refactor day 19 part 1

--- typescript/day_19.ts
+++ typescript/day_19.ts
@@ -1,7 +1,14 @@
 import { printSolution, readFile } from './util'
 
-function part1 (rules: Map<string, RegExp>, messages: Array<string>): number {
-  const ruleZero = rules.get('0')
+type RawRules = Map<string, Array<string>>;
+
+function part1 (rules: RawRules, messages: Array<string>): number {
+  const ruleMap = new Map(
+    Array.from(rules.entries())
+      .map(([n, rule]) => [n, constructRegExp(rules, rule)]),
+  )
+
+  const ruleZero = ruleMap.get('0')
   if (ruleZero === undefined) {
     throw new Error('no rule 0!')
   }
@@ -9,8 +16,35 @@ function part1 (rules: Map<string, RegExp>, messages: Array<string>): number {
   return messages.filter(message => message.match(ruleZero)).length
 }
 
+function part2 (rules: RawRules, messages: Array<string>): number {
+  const ruleMap = new Map(
+    Array.from(rules.entries())
+      .map(([n, rule]) => [n, constructRegExp(rules, rule)]),
+  )
+
+  const ruleZero = ruleMap.get('0')
+  if (ruleZero === undefined) {
+    throw new Error('no rule 0!')
+  }
+
+  return messages.filter(message => message.match(ruleZero)).length
+}
+
+function constructRegExp (rules: RawRules, rule: Array<string>): RegExp {
+  const parts = [...rule]
+  while (parts.some(c => rules.has(c))) {
+    for (const [idx, part] of parts.entries()) {
+      if (rules.has(part)) {
+        parts.splice(idx, 1, '(', ...rules.get(part) || part, ')')
+        break
+      }
+    }
+  }
+  return RegExp(`^${parts.join('')}$`)
+}
+
 const [rawRules, rawMessages] = readFile('data/day_19.txt').split('\n\n')
-const rules: Map<string, Array<string>> = new Map(
+const rules: RawRules = new Map(
   rawRules
     .split('\n')
     .map(line => {
@@ -26,21 +60,8 @@ const rules: Map<string, Array<string>> = new Map(
     },
     ),
 )
-const rules2: Array<[string, RegExp]> = Array.from(rules.entries())
-  .map(([n, rule]) => {
-    const parts = [...rule]
-    while (parts.some(c => rules.has(c))) {
-      for (const [idx, part] of parts.entries()) {
-        if (rules.has(part)) {
-          parts.splice(idx, 1, '(', ...rules.get(part) || part, ')')
-          break
-        }
-      }
-    }
-    return [n, RegExp(`^${parts.join('')}$`)]
-  })
-const ruleMap = new Map(rules2)
 
 const messages = rawMessages.split('\n')
 
-printSolution(19, 1, part1(ruleMap, messages))
+printSolution(19, 1, part1(rules, messages))
+printSolution(19, 2, part2(rules, messages))
