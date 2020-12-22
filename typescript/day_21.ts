9f2832afc10eb4adac1fc1c53807be17b32048d0
Josh Karpel <josh.karpel@gmail.com>
2020-12-22 17:32:46 -0500

    solve day 21

--- typescript/day_21.ts
+++ typescript/day_21.ts
@@ -1,36 +1,55 @@
-import { printSolution, readFile, regExtract } from './util'
+import { difference, intersection, printSolution, readFile, regExtract } from './util'
 
-type Food = {ingredients: Array<string>, allergens: Array<string>}
+type Food = { ingredients: Array<string>, allergens: Array<string> }
 type Foods = Array<Food>
 
-function part1 (foods: Foods): number {
-  console.log(foods)
-
-  const allergenToPossibleIngredients: Map<string, Set<string>> = new Map()
-  const ingredientToPossibleAllergens: Map<string, Set<string>> = new Map()
+function determineAllergenIngredients (foods: Array<Food>): Map<string, string> {
+  const possibleIngredientsMap: Map<string, Set<string>> = new Map()
 
   for (const food of foods) {
     for (const allergen of food.allergens) {
-      const possibilities = allergenToPossibleIngredients.get(allergen) || new Set()
-      for (const ingredients of food.ingredients) {
-        possibilities.add(ingredients)
-      }
-      allergenToPossibleIngredients.set(allergen, possibilities)
-    }
+      const possibilities: Set<string> = intersection(
+        possibleIngredientsMap.get(allergen) || food.ingredients,
+        food.ingredients,
+      )
 
-    for (const ingredient of food.ingredients) {
-      const possibilities = ingredientToPossibleAllergens.get(ingredient) || new Set()
-      for (const allergens of food.allergens) {
-        possibilities.add(allergens)
-      }
-      ingredientToPossibleAllergens.set(ingredient, possibilities)
+      possibleIngredientsMap.set(allergen, possibilities)
     }
   }
 
-  console.log(allergenToPossibleIngredients)
-  console.log(ingredientToPossibleAllergens)
+  const possibleIngredients = Array.from(possibleIngredientsMap.entries())
+    .sort(([_f1, s1], [_f2, s2]) => s1.size - s2.size)
 
-  return 0
+  const ingredientToAllergen = new Map()
+  while (ingredientToAllergen.size < possibleIngredients.length) {
+    for (const [allergen, possibilities] of possibleIngredients) {
+      const remainingPossibilities = difference(possibilities, ingredientToAllergen.keys())
+      if (remainingPossibilities.size === 1) {
+        const ingredient = remainingPossibilities.values().next().value as number
+        ingredientToAllergen.set(ingredient, allergen)
+      }
+    }
+  }
+
+  return ingredientToAllergen
+}
+
+function part1 (foods: Foods): number {
+  const ingredientToAllergen = determineAllergenIngredients(foods)
+
+  return foods
+    .flatMap(food => food.ingredients)
+    .filter(ingredient => !ingredientToAllergen.has(ingredient))
+    .length
+}
+
+function part2 (foods: Foods): string {
+  const ingredientToAllergen = determineAllergenIngredients(foods)
+
+  return Array.from(ingredientToAllergen.entries())
+    .sort(([_i1, a1], [_i2, a2]) => a1.localeCompare(a2))
+    .map(([ingredient, _allergen]) => ingredient)
+    .join(',')
 }
 
 const foods = readFile('data/day_21.txt')
@@ -44,3 +63,4 @@ const foods = readFile('data/day_21.txt')
   })
 
 printSolution(21, 1, part1(foods))
+printSolution(21, 2, part2(foods))
